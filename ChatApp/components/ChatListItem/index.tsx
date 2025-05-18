a1f83152340fdb8e15af651012692e72765135cc
ilonivna <147661751+ilonivna@users.noreply.github.com>
2025-05-18 20:39:10 +0300

    message date display fixed

--- ChatApp/components/ChatListItem/index.tsx
+++ ChatApp/components/ChatListItem/index.tsx
@@ -2,6 +2,25 @@ import { ChatRoom } from "@/types";
 import React from "react";
 import { Image, Text, View } from "react-native";
 import styles from "./styles";
+import moment from 'moment';
+
+
+const formatMessageTime = (timestamp: string) => {
+  const messageDate = moment(timestamp);
+  const now = moment();
+
+  if (messageDate.isSame(now, 'day')) {
+    return 'Today';
+  } else if (messageDate.isSame(now.clone().subtract(1, 'day'), 'day')) {
+    return 'Yesterday';
+  } else if (now.diff(messageDate, 'days') < 7) {
+    return messageDate.format('dddd'); 
+  } else {
+    return messageDate.format('DD/MM/YYYY'); 
+  }
+};
+
+
 
 export type ChatListItemProps = {
   chatRoom: ChatRoom;
@@ -17,11 +36,11 @@ const ChatListItem = (props: ChatListItemProps) => {
 
         <View style={styles.midCont}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.content}>{chatRoom.lastMessage.content}</Text>
+          <Text numberOfLines={2} style={styles.content}>{chatRoom.lastMessage.content}</Text>
         </View>
       </View>
-      {/* <Text style={styles.time}>{chatRoom.lastMessage.createdAt}</Text> */}
-      <Text style={styles.time}>Today</Text>
+      <Text style={styles.time}>{formatMessageTime(chatRoom.lastMessage.createdAt)}</Text>
+      
     </View>
   );
 };
